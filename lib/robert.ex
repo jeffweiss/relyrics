f5fefe3a3a54e2f76ce69016fbbf4a650fab0072
Jeff Weiss <jeff.weiss@puppetlabs.com>
2015-07-03 18:00:14 -0700

    dynamically functions for each character head
    Prior to this commit, we had no way of producing a list of possible
    words for a sorted character head; i.e. a sorted list of the characters
    in the word. This list will include anagrams like `%{'em' => ["em",
    "me"]}`. At compile-time we generate a static function for each
    character head that statically points to the list of possible words,
    thus ensuring that the dictionary is only consulted at compile-time, and
    never at run time. An `@external_resource` has been established so that
    if the dictionary changes, mix will know to recompile the `Robert`
    module.
    
    Here's an example of what it looks like in action:
    ```
    Interactive Elixir (1.0.4) - press Ctrl+C to exit (type h() ENTER for
    help)
    iex(1)> Robert.possible_words('em')
    ["me", "em"]
    iex(2)>
    ```

--- lib/robert.ex
+++ lib/robert.ex
@@ -1,4 +1,6 @@
 defmodule Robert do
+
+  @external_resource dictionary_path = Path.join([__DIR__, "..", "data", "OTP_dictionary.txt"])
   use GenServer
 
   def start_link do
@@ -6,6 +8,15 @@ defmodule Robert do
   end
 
   def init do
+    {:ok, nil}
   end
 
+  analyzed_dictionary = File.stream!(dictionary_path)
+  |> Stream.map(&String.strip/1)
+  |> Stream.map(&( {&1 |> String.to_char_list |> Enum.sort, &1}))
+  |> Enum.reduce(%{}, fn ({char_head, word}, map) -> list = Map.get(map, char_head, []); Map.put(map, char_head, [word|list]) end)
+
+  for {char_head, word_list} <- analyzed_dictionary do
+    def possible_words(unquote(char_head)), do: unquote(word_list)
+  end
 end
